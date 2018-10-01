7f0569015d65a1b8686bf75243f11b16b6410cd3
Servio Palacios <servio@palacios.com>
2018-10-01 10:35:16 -0400

    [IFT api - constructor receives apiKey and organizationid]

--- src/ift/ift.js
+++ src/ift/ift.js
@@ -1,6 +1,6 @@
 "use strict";
 
- /* Copyright 2018 Open Ag Data Alliance
+/* Copyright 2018 Open Ag Data Alliance
  *
  * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
@@ -15,110 +15,124 @@
  * limitations under the License.
  */
 
- /**
+/**
  * @author Servio Palacios
  * IFT - IBM Food Trust Derived Class. IFT = IBM Food Trust
  * @module src/ift/ift
  *
  */
 const REST = require("./rest");
-const pretty = require('prettyjson');
+const pretty = require("prettyjson");
 
-class IFT extends REST{
+class IFT extends REST {
+  constructor(param = {}) {
+    super(param);
+    let self = this;
+  } //constructor
 
-    constructor(param = {}){
-        super();
-        let self = this;
-    }//constructor
+  /**
+   * add a new certificate in the ledger
+   * it converts the prmiusgfs format to IFT format
+   * then it send the content to the IFT
+   * @param {*} _audit, _certificate
+   */
+  putCertificate(_audit, _certificate) {
+    let self = this;
+    console.log("[ADDING] - [CERTIFICATE]");
+    if (self._connected) {
+      if (!self._onboarding_token_in_header) {
+        self._buildCertificatesHeader();
+      }
 
-    /**
-     * add a new certificate in the ledger
-     * it converts the prmiusgfs format to IFT format
-     * then it send the content to the IFT
-     * @param {*} _audit, _certificate  
-     */
-    putCertificate(_audit, _certificate){
-        let self = this;
-        console.log("[ADDING] - [CERTIFICATE]");
-        if(self._connected){
-            if(!self._onboarding_token_in_header){
-                self._buildCertificatesHeader();
-            }
+      return self
+        .post(
+          self._certificates_path,
+          self._certificates_header,
+          "",
+          self._mapOada2Hyperledger(_audit, _certificate)
+        )
+        .then(response => {
+          //console.log('newCertificate', response);
+          return Promise.resolve(response.data.certificationId);
+        })
+        .catch(err => {
+          return Promise.reject;
+        });
+    } else {
+      console.log("[Not connected]");
+    }
+  } //addCertificate
 
-            return self.post(self._certificates_path, self._certificates_header, "", 
-                             self._mapOada2Hyperledger(_audit, _certificate)).then((response) => {
-                //console.log('newCertificate', response);
-                return Promise.resolve(response.data.certificationId);
-            }).catch((err) => {
-                return Promise.reject;
-            });
-        }
-        else{
-            console.log("[Not connected]");
-        }
-    }//addCertificate
+  /**
+   * deletes a certificate
+   */
+  delCertificate(_certificationId) {
+    let self = this;
+    console.log("[DELETING] - [CERTIFICATE]");
+    if (self._connected) {
+      if (!self._onboarding_token_in_header) {
+        self._buildCertificatesHeader();
+      }
+      let _path =
+        self._certificates_point_query_path +
+        self._certificate_id +
+        self._certificate_delete_path_leftover;
 
-    /**
-     * deletes a certificate
-     */
-    delCertificate(_certificationId){
-        let self = this;
-        console.log("[DELETING] - [CERTIFICATE]");
-        if(self._connected){
-            if(!self._onboarding_token_in_header){
-                self._buildCertificatesHeader();
-            }
-            let _path = self._certificates_point_query_path + self._certificate_id + self._certificate_delete_path_leftover;
-            
-            self.del(_path, self._certificates_header).then((response) => {
-                console.log('delCertificates', response);
-            });
-        }
-        else{
-            console.log("[Not connected]");
-        }
-    }//deleteCertificate
+      self.del(_path, self._certificates_header).then(response => {
+        console.log("delCertificates", response);
+      });
+    } else {
+      console.log("[Not connected]");
+    }
+  } //deleteCertificate
 
-    /**
-     * returns the content of the _certificationId stored in the hyperledger
-     * @param {*} _certificationId 
-     */
-    getCertificate(_certificationId){
-        let self = this;
-        console.log("[FETCHING] - [CERTIFICATE]");
-        if(self._connected){
-            if(!self._onboarding_token_in_header){
-                self._buildCertificatesHeader();
-            }
-            let _path = _certificationId ? self._certificates_point_query_path + _certificationId : self._certificates_path;
-            //console.log("Certificates Header", self._certificates_header);
-            self.get(_path, self._certificates_header).then((response) => {
-                console.log('getCertificates', response);
-                return response.data;
-            }).catch( (err) => {
-                console.log("[Error]");
-                return null;
-            });
-        }
-        else{
-            console.log("[Not connected]");
-        }
-    }//getCertificate
+  /**
+   * returns the content of the _certificationId stored in the hyperledger
+   * @param {*} _certificationId
+   */
+  getCertificate(_certificationId) {
+    let self = this;
+    console.log("[FETCHING] - [CERTIFICATE]");
+    if (self._connected) {
+      if (!self._onboarding_token_in_header) {
+        self._buildCertificatesHeader();
+      }
+      let _path = _certificationId
+        ? self._certificates_point_query_path + _certificationId
+        : self._certificates_path;
+      //console.log("Certificates Header", self._certificates_header);
+      self
+        .get(_path, self._certificates_header)
+        .then(response => {
+          console.log("getCertificates", response);
+          return response.data;
+        })
+        .catch(err => {
+          console.log("[Error]");
+          return null;
+        });
+    } else {
+      console.log("[Not connected]");
+    }
+  } //getCertificate
 
-    /**
-     *  waits for connection to be established then sends request for a certificate
-     **/
-    getCertificateManager(_certificationId){
-        let self = this;
-        if(!self._connected){
-            setTimeout(self.getCertificate(_certificationId).then((response) => {console.log(response)}), 100); 
-        }
-        else{
-            return self.getCertificate();
-        }
-    }//getCertificateManager
-
-}//class
+  /**
+   *  waits for connection to be established then sends request for a certificate
+   **/
+  getCertificateManager(_certificationId) {
+    let self = this;
+    if (!self._connected) {
+      setTimeout(
+        self.getCertificate(_certificationId).then(response => {
+          console.log(response);
+        }),
+        100
+      );
+    } else {
+      return self.getCertificate();
+    }
+  } //getCertificateManager
+} //class
 
 /* exporting the module */
 module.exports = IFT;
--- src/ift/index.js
+++ src/ift/index.js
@@ -1,17 +1,23 @@
-const pretty = require('prettyjson');
+const pretty = require("prettyjson");
 const primusgfs = require("./primusgfs");
 
 const IFT = require("./ift");
 
-let _IFT = new IFT({});
+let _IFT = new IFT({
+  organization_id: "",
+  apiKey: ""
+});
 
 /**
  * connecting to IFT framework
  * adding a new certificate to the IFT
  */
-_IFT.connect().then( (response) => {
-    _IFT.putCertificate(primusgfs).then((response) => {console.log(response)}); 
-    //console.log("New Certification ID",_IFT.putCertificate(primusgfs));
+_IFT.connect().then(response => {
+  _IFT.getCertificateManager("e623ec02f73ce20428201045b1f68df5");
+  //   _IFT.putCertificate(primusgfs, primusgfs).then(response => {
+  //     console.log(response);
+  //   });
+  //console.log("New Certification ID",_IFT.putCertificate(primusgfs));
 });
 // _IFT.connect().then((response) => {
 //     //_IFT.getCertificateManager(certificationId);
@@ -23,4 +29,3 @@ _IFT.connect().then( (response) => {
  * converting primusgfs to IFT schema
  */
 //console.log(_IFT._mapOada2Hyperledger(primusgfs));
-
