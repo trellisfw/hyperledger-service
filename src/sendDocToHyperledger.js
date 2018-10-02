808070874035628b87297a9d140cfe6f30e85ba1
Cyrus Bowman <cyrusbow@gmail.com>
2018-10-02 14:16:16 -0400

    - IFT library now gets new token if it has expired - Cleaned up code in IFT library - IFT library no longer saves token to file.

--- src/sendDocToHyperledger.js
+++ src/sendDocToHyperledger.js
@@ -14,24 +14,20 @@ const ift = new _IFT({
 function sendDocToHyperledger({token, certificationMeta, audit, certificate}) {
   const certificationId = certificationMeta._id.replace('resources/', '').replace('/_meta', '');
   //Send to hyperledger using ift
-  return ift.connect().then(() => {
-    return ift.putCertificate(audit, certificate).then((hyperledgerId) => {
-      //Add hyperledger_id to certification
-      debug(certificationId, 'Successfully pushed to hyperledger. hyperledger_id:', hyperledgerId);
-      return axios({
-        method: 'PUT',
-        url: config.api+'/'+certificationMeta._id+'/hyperledger_id',
-        headers: {
-          Authorization: 'Bearer '+token,
-          'Content-Type': 'application/json'
-        },
-        data: JSON.stringify(hyperledgerId)
-      });
-    }).catch((err) => {
-      debug(certificationId, 'Error: Failed to putCertificate() to hyperledger.', err);
+  return ift.putCertificate(audit, certificate).then((hyperledgerId) => {
+    //Add hyperledger_id to certification
+    debug(certificationId, 'Successfully pushed to hyperledger. hyperledger_id:', hyperledgerId);
+    return axios({
+      method: 'PUT',
+      url: config.api+'/'+certificationMeta._id+'/hyperledger_id',
+      headers: {
+        Authorization: 'Bearer '+token,
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify(hyperledgerId)
     });
   }).catch((err) => {
-    debug(certificationId, 'Error: Failed to connect to hyperledger.', err);
+    debug(certificationId, 'Error: Failed to putCertificate() to hyperledger.', err);
   });
 }
 
