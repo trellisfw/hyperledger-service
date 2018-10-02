4c2cbd64c7604d40594da8d2e488402311a7693f
sanoel <noel.samuel.a@gmail.com>
2018-10-02 16:50:25 -0400

    testing

--- test/ift.test.js
+++ test/ift.test.js
@@ -1,14 +1,15 @@
 var mocha = require('mocha');
 var _ = require('lodash');
-var ift = require('@trellisfw/ift')
+var ift = require('../src/ift/ift');
 var Promise = require('bluebird');
 var axios = require('axios');
 var {expect}= require('chai');
-var {gln, token} = require('./config.js');
+var config = require('./config.js');
 var pgfsTemplate = require('./pgfsTemplate');
 var makeCertification = require('./makeCertification');
 var result;
 var certificationId;
+var _IFT;
 
 async function cleanUp() {
   await oadaRequest({
@@ -34,7 +35,7 @@ function oadaRequest({method, url, data, type}) {
     method,
     url,
     headers: {
-      'Authorization': 'Bearer '+token,
+      'Authorization': 'Bearer '+config.token,
     }
   }
   if (type) request.headers['Content-Type'] = type;
@@ -45,6 +46,11 @@ function oadaRequest({method, url, data, type}) {
 describe('Check trellis signatures service', () => {
 
   describe('Should create certifications, a certification, certificate, and audit first', async function() {
+    before('Connect to IBM Food Trust', async function() {
+      _IFT = new ift(config.ift)
+      await _IFT.connect()
+    })
+
     it('Create test certification', async function() {
       this.timeout(5000);
       var audit = pgfsTemplate;
@@ -55,29 +61,29 @@ describe('Check trellis signatures service', () => {
       }
       delete certificate.sections;
       delete certificate.control_points;
-      certificate.organization.GLN = gln;
+      certificate.organization.GLN = config.gln;
       result = await makeCertification(audit, certificate)
       certificationId = result.certification_id.replace(/^resources\//, '');
-      console.log(result)
     })
 
     it('Should create a hyperledger_id on the certification and in Hyperledger itself', async function() {
       this.timeout(65000)
       this.retries(7);
       await Promise.delay(5000)
+      console.log('https://api.trellis.one/bookmarks/certifications/'+certificationId+'/_meta')
       var certificationMeta = await oadaRequest({
         method: 'get',
         url: 'https://api.trellis.one/bookmarks/certifications/'+certificationId+'/_meta',
       })
-
       expect(certificationMeta.data).to.include.keys('hyperledger_id')
+      var hyperledgerAudit = await _IFT.getCertificate(certificationMeta.data.hyperledger_id)
+      expect(hyperledgerAudit.metadata).to.include.keys(['locationGLNList'])
     })
-
     it('Clean Up', async function() {
       cleanUp()
     })
   })
-
+    /*
   describe(`Shouldn't put certifications into hyperledger that are missing a GLN on the audit/certificate`, async function() {
     it(`Create test certification`, async function() {
       this.timeout(5000);
@@ -138,4 +144,6 @@ describe('Check trellis signatures service', () => {
       expect(certificationMeta.data).not.to.include.keys('hyperledger_id')
     })
   })   
+    */
+
 })
