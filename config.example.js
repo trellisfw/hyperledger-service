dc937de26b8804d3ed010cd7d76015242bf9ab53
Servio Palacios <servio@palacios.com>
2018-10-02 22:29:56 -0400

    [ift-api includes analytics_url and custom location/gln]

--- config.example.js
+++ config.example.js
@@ -1,17 +1,15 @@
 module.exports = {
-  api: 'https://a.trellis.api.here',
-  tokensFile: './tokens.json',
+  api: "https://a.trellis.api.here",
+  tokensFile: "./tokens.json",
   interval: 30, //seconds
-  supportedTypes: [
-    'application/vnd.trellis.certification.primusgfs.1+json'
-  ],
+  supportedTypes: ["application/vnd.trellis.certification.primusgfs.1+json"],
   ift: {
-    organizationId: 'someIdHere',
-    apiKey: 'someSecretKeyHere'
+    organizationId: "someIdHere",
+    apiKey: "someSecretKeyHere"
   },
   debug: {
     sendToHyperledger: true,
-    overrideGLN: '0728612177446',
+    overrideGLN: "1234567890123",
     verifySignatures: true
   }
-}
+};
